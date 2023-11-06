a08b1a9649ab16c81350ba42a35e5d8559b8ca9b
Leonardo Mercado <81432796+LeoMerc@users.noreply.github.com>
2023-11-06 15:28:24 -0800

    database filled

--- collection/models.py
+++ collection/models.py
@@ -2,28 +2,28 @@ from django.db import models
 
 
 class Artist(models.Model):
-    slug = models.SlugField(max_length=80, unique=True)
-    name = models.CharField(max_length=80)
-    born_date = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    born_date = models.CharField(max_length=200)
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=40, unique=True)
+    name = models.CharField(max_length=200, unique=True)
 
 
 class Style(models.Model):
-    name = models.CharField(max_length=40, unique=True)
+    name = models.CharField(max_length=200, unique=True)
 
 
 class Period(models.Model):
-    name = models.CharField(max_length=40, unique=True)
+    name = models.CharField(max_length=200, unique=True)
 
 
 class Artwork(models.Model):
     author = models.ForeignKey(Artist, on_delete=models.RESTRICT)
-    path = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    date = models.CharField(max_length=40, null=True)
+    path = models.CharField(max_length=500)
+    title = models.CharField(max_length=500)
+    date = models.CharField(max_length=200, null=True)
     style = models.ForeignKey(Style, null=True, on_delete=models.RESTRICT)
     period = models.ForeignKey(Period, null=True, on_delete=models.RESTRICT)
     genre = models.ForeignKey(Genre, null=True, on_delete=models.RESTRICT)
